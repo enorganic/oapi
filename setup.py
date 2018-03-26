c417a035bca1032335727ad3a6a559d750baae60
David Belais <davebelais@gmail.com>
2018-03-26 15:05:52 -0700

    *refactoring

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='oapi',
 
-    version='0.0.20',
+    version='0.0.21',
 
     description='An SDK for parsing OpenAPI (Swagger) 2.0 - 3.0 specifications.',
 
