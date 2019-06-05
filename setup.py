99950570ea606ae2320c96bd25d35362c209fc58
David Belais <davebelais@gmail.com>
2019-06-05 13:25:41 -0700

    *

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='oapi',
 
-    version="0.0.30",
+    version="0.0.32",
 
     description='An SDK for parsing OpenAPI (Swagger) 2.0 - 3.0 specifications.',
 
